65993a3f81db3ff13dc0b9584e00d07c77dc950e
Leandro Cruz <leandro.saad@gmail.com>
2026-04-10 16:33:34 -0300

    Added /error to SampleApp

--- modules/sample/src/main/scala/guara/sample/SampleApp.scala
+++ modules/sample/src/main/scala/guara/sample/SampleApp.scala
@@ -4,19 +4,28 @@ import guara.framework.GuaraApp
 import guara.framework.background.BackgroundServices
 import guara.framework.router.{Echo, Router}
 import guara.http.*
-import guara.http.extensions.*
+import guara.http.errors.ReturnErrorCodeWithMessage
 import zio.*
 import zio.http.*
-import zio.json.*
 import zio.logging.backend.SLF4J
 
 object SampleApp extends GuaraApp {
 
   override val bootstrap = Runtime.removeDefaultLoggers >>> SLF4J.slf4j
 
+  given Origin = Origin.of("SampleApp")
+
+  private def forcedError(request: Request) = ensureResponse {
+    ZIO.fail(ReturnErrorCodeWithMessage(0, "Forced Error"))
+  }.toTask
+
+  private val ours = Routes(
+    Method.GET / "error" -> Handler.fromFunctionZIO[Request](forcedError),
+  ).sandbox
+
   val router = ZLayer.succeed {
     new Router {
-      override def routes = Echo.routes
+      override def routes = Echo.routes ++ ours
     }
   }
 
